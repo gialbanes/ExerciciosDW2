5eb727ed95f79597b2367745dab3775815b02080
DSM2 <fatec-dsm2@academico.intra>
2024-08-28 19:25:26 -0300

    aula 28-08

--- javascript/js-aula-0.3-classe-data-moeda.js
+++ javascript/js-aula-0.3-classe-data-moeda.js
@@ -64,10 +64,54 @@ console.log(`Hoje é dia ${dia}!`);
 
 //PEGAR O MÊS ATUAL
 const mes = dataAtual.getMonth() + 1;
-console.log(`Estamos no mês ${mes}!`)
+console.log(`Estamos no mês ${mes}!`);
 
 //PEGAR O ANO ATUAL
 const ano = dataAtual.getFullYear();
-console.log(`Estamos no ano ${ano}!`)
+console.log(`Estamos no ano ${ano}!`);
 
-console.log(`Hoje é ${dia}/${mes}/${ano}.`)
+console.log(`Hoje é ${dia}/${mes}/${ano}.`);
+
+console.log("----------------------------------------------------");
+
+//MANIPULANDO MOEDAS NO JS
+let salario = 2500.3;
+console.log(salario);
+//TO-FIXED - DEFINE O NUMERO DE CASAS DECIMAIS
+console.log(salario.toFixed(2));
+//REPLACE - ALTERA O FORMATO DA CASA DECIAMAL
+console.log(salario.toFixed(2).replace(".", ","));
+// toLocaleString -faz a formatação para determinada moeda
+//REAL
+console.log(
+  salario.toLocaleString("pt-br", { style: "currency", currency: "BRL" })
+);
+//DÓLAR
+console.log(
+  salario.toLocaleString("en", { style: "currency", currency: "USD" })
+);
+//EURO
+console.log(
+  salario.toLocaleString("pt-br", { style: "currency", currency: "EUR" })
+);
+
+//CONVERTENDO REAL - DÓLAR
+let salarioDolar = salario * 0.18;
+console.log(
+  salarioDolar.toLocaleString("en", { style: "currency", currency: "USD" })
+);
+
+console.log("----------------------------------------------------");
+
+//FORMATANDO STRINGS
+const nome = "Giovana Albanês";
+//LETRAS MAIÚSCULAS
+console.log(nome.toUpperCase());
+//LETRAS MINÚSCULAS
+console.log(nome.toLowerCase());
+//CONTAR LETRAS - LENGHT
+console.log(nome.length);
+
+//PODEMOS USAR O LENGHT PARA ARRAYS
+const lista = ["Maça", "Banana", "Morango"];
+console.log(lista.length);
