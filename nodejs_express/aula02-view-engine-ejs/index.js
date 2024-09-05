db34eff044da7a4139150184bb63b8af9cb71b15
DSM2 <fatec-dsm2@academico.intra>
2024-09-05 19:30:17 -0300

    aula 05-09 completa

--- nodejs_express/aula02-view-engine-ejs/index.js
+++ nodejs_express/aula02-view-engine-ejs/index.js
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
 app.get("/perfil/:nome?", (req, res) => {
   const nome = req.params.nome;
   res.render("perfil", {
-    nome: nome
+    nome: nome,
   });
 });
 
@@ -30,26 +30,42 @@ app.get("/videos/:playlist?/:video?", (req, res) => {
   const video = req.params.video;
   res.render("videos", {
     playlist: playlist,
-    video: video
+    video: video,
   });
 });
 
 //ROTA DE PRODUTOS
 app.get("/produtos/:produto?", (req, res) => {
-  const listaProdutos = ['Computador', ' Tablet', ' Celular', ' Notebook'];
+  const listaProdutos = ["Computador", " Tablet", " Celular", " Notebook"];
   const produto = req.params.produto;
   res.render("produtos", {
     produto: produto,
-    listaProdutos : listaProdutos //NA PÁGINA PRODUTOS.EJS HAVERÁ UM TESTE DE CONDIÇÃO
+    listaProdutos: listaProdutos, //NA PÁGINA PRODUTOS.EJS HAVERÁ UM TESTE DE CONDIÇÃO
+  });
+});
+
+//ROTA PEDIDOS
+app.get("/pedidos", (req, res) => {
+  //ARRAY DE OBJETOS COM OS PEDIDOS
+  const pedidos = [
+    { produto: "Celular", valor: 3000 },
+    { produto: "Computador", valor: 4000 },
+    { produto: "Tablet", valor: 2000 },
+    { produto: "Notebook", valor: 3800 },
+  ];
+  res.render("pedidos", {
+    //ENVIANDO O ARRAY P PÁGINA
+    pedidos: pedidos,
   });
 });
 
 //INICIANDO O SERVIDOR NA PORTA 8080; ELE RECEBRÁ AS REQUISIÇÕES E ENVIARÁ AS RESPOSTAS
-app.listen(8080, (error) => {
+const port = 8080;
+app.listen(port, (error) => {
   //PORTA PADRÃO; FUNÇÃO PARA TRATAR UM POSSÍVEL ERRO
   if (error) {
     console.log(`Ocorreu um erro: ${error}`);
   } else {
-    console.log("Servidor iniciado com sucesso!");
+    console.log(`Servidor iniciado com sucesso em: http://localhost:${port}`);
   }
 });
