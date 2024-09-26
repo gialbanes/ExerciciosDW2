0b4e69a409d981a38f651369ba6cd28f4725a9cc
DSM2 <fatec-dsm2@academico.intra>
2024-09-26 19:09:00 -0300

    aula 25-09 completa

--- nodejs_express/aula-05-SistemaLoja/controllers/ProdutosController.js
+++ nodejs_express/aula-05-SistemaLoja/controllers/ProdutosController.js
@@ -1,17 +1,41 @@
-import express from 'express'
-const router = express.Router()
+import express from "express";
+const router = express.Router();
+import Produto from "../models/Produto.js";
 
 // ROTA PRODUTOS
-router.get("/produtos", function(req,res){
-    const produtos = [
-        {nome: "Celular Motorola E22", preco: 1200, categoria: "Eletroportáteis"},
-        {nome: "Tablet Samsung", preco: 900, categoria: "Eletrônicos"},
-        {nome: "Notebook Lenovo", preco: 3200, categoria: "Computadores"},
-        {nome: "Fone Bluetooth", preco: 150, categoria: "Periféricos"}
-    ]
+router.get("/produtos", function (req, res) {
+  Produto.findAll().then((produtos) => {
     res.render("produtos", {
-        produtos: produtos
-    })
-})
+      produtos: produtos,
+    });
+  });
+});
 
-export default router
+router.post("/produtos/new", (req, res) => {
+  const nome = req.body.nome;
+  const preco = req.body.preco;
+  const categoria = req.body.categoria;
+  Produto.create({
+    nome: nome,
+    preco: preco,
+    categoria: categoria,
+  }).then(() => {
+    res.redirect("/produtos");
+  });
+});
+
+router.get("/produtos/delete/:id", (req, res) => {
+  const id = req.params.id;
+  Produto.destroy({
+    where: {
+      id: id,
+    },
+  })
+    .then(() => {
+      res.redirect("/produtos");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+export default router;
