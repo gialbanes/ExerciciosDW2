33dc5dfe253eae737e58a9f784bc7f2b8a652e98
gialbanes <albanesgiovana@gmail.com>
2024-11-07 19:29:23 -0300

    aula 07-11 completa

--- nodejs_express/aula07-upload-de-arquivos/index.js
+++ nodejs_express/aula07-upload-de-arquivos/index.js
@@ -1,27 +1,57 @@
 import express from "express";
-const app = express();
 import multer from "multer";
+import connection from "./config/sequelize-config.js";
+import Galeria from "./models/Galeria.js";
+const app = express();
 
-// Configurações 
-app.use(express.static('public'))
-app.set('view engine', 'ejs')
+//REALIZANDO A CONEXÃO COM O BANCO DE DADOS
+connection
+  .authenticate()
+  .then(() => {
+    console.log("Conexão com o banco de dados feita com sucesso!");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
-const upload = multer({dest: "public/uploads/"})
+//CRIANDO O BANCO DE DADOS SE ELE NÃO EXISTIR
+connection
+  .query(`CREATE DATABASE IF NOT EXISTS galeria;`)
+  .then(() => {
+    console.log("Banco de dados já criado.");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+// Configurações
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+const upload = multer({ dest: "public/uploads/" });
 
 app.get("/", (req, res) => {
-    res.render("index")
-})
+  Galeria.findAll().then((imagens) => {
+    res.render("index", {
+      imagens: imagens,
+    });
+  });
+});
 
 // rota de upload
 app.post("/upload", upload.single("file"), (req, res) => {
-    res.redirect("/")
-})
+  const file = req.file.filename;
+  Galeria.create({
+    file: file,
+  });
+  res.redirect("/");
+});
 
 const port = 8081;
 app.listen(port, (error) => {
-    if(error){
-        console.log(`Ocorreu um erro ${error}`)
-    } else {
-        console.log(`Servidor iniciado com sucesso em: http://localhost:${port}.`)
-    }
-})
+  if (error) {
+    console.log(`Ocorreu um erro ${error}`);
+  } else {
+    console.log(`Servidor iniciado com sucesso em: http://localhost:${port}.`);
+  }
+});
